dffdff86ef8b19f078c1b37070710e701d6806db
vermout94 <david.berger4494@gmail.com>
2023-06-15 19:36:36 +0200

    initial commit

---
+++ src/main/java/at/technikum/SLM/SlmApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.SLM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlmApplication.class, args);
+	}
+
+}
